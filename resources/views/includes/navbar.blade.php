69e7ef007961387bd224e4edbfb4d190a1b702db
arislamp <129835271+arislamp@users.noreply.github.com>
2023-10-28 18:43:15 +0300

    minor css fixes

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,14 +3,14 @@
           <h2><a href="{{ route('home') }}">Cinerama</a></h2>
           <ul class="navbar-links">
               @if (Auth::check() && Auth::user()->role == 'admin')
-                  <li><a href="{{ route('add_movie') }}">Προσθήκη Ταινίας</a></li>
-                  <li><a href="{{ route('schedule') }}">Διαχείρηση Ημερομηνίων</a></li>
-                  <li><a href="{{ route('show_reservations') }}">Κρατήσεις</a></li>
-                  <li><a href="{{ route('movies') }}">Ταινίες</a></li>
+                  <li><a class="{{ request()->is('add_movie') ? 'active-link' : '' }}" href="{{ route('add_movie') }}">Προσθήκη Ταινίας</a></li>
+                  <li><a class="{{ request()->is('schedule') ? 'active-link' : '' }}" href="{{ route('schedule') }}">Διαχείρηση Ημερομηνίων</a></li>
+                  <li><a class="{{ request()->is('show_reservations') ? 'active-link' : '' }}" href="{{ route('show_reservations') }}">Κρατήσεις</a></li>
+                  <li><a class="{{ request()->is('movies') ? 'active-link' : '' }}" href="{{ route('movies') }}">Ταινίες</a></li>
               @endif
               @if (Auth::check() && Auth::user()->role == 'customer')
-                  <li><a href="{{ route('my_reservations') }}">Οι Κρατήσεις μου</a></li>
-                  <li><a href="{{ route('movies') }}">Ταινίες</a></li>
+                  <li><a class="{{ request()->is('my_reservations') ? 'active-link' : '' }}" href="{{ route('my_reservations') }}">Οι Κρατήσεις μου</a></li>
+                  <li><a class="{{ request()->is('movies') ? 'active-link' : '' }}" href="{{ route('movies') }}">Ταινίες</a></li>
               @endif
           </ul>
           @if (!Auth::check())
